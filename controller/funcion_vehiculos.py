a1a79a549b4ac203f8104e8841ab821f95f377b3
rodrigoavitia <rodrigo_3141240156@utd.edu.mx>
2025-11-28 07:33:00 -0600

    Delete controller/funcion_vehiculos.py

--- controller/funcion_vehiculos.py
+++
@@ -1,9 +0,0 @@
-from tkinter import messagebox
-import os
-from model import vehiculo
-from conexionBD import *
-os.system("cls")
-
-def registrar_vehiculo(marca, modelo, color, placa, anio, id_usuario):
-    vehiculo.Consultas.registrar_vehiculo(marca, modelo, color, placa, anio, id_usuario)
-    
