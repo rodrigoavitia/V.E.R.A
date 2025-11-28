01a11d9c3b2038267cc2f683a68a2bc458a66e16
rodrigoavitia <rodrigo_3141240156@utd.edu.mx>
2025-11-28 07:33:15 -0600

    Delete controller directory

--- controller/funcion_imagenes.py
+++
@@ -1,9 +0,0 @@
-from tkinter import messagebox
-import os
-from model import imagenes
-from conexionBD import *
-os.system("cls")
-
-
-def registrar_imagenes(imagen, tipo, fecha, hora):
-    imagenes.Consultas.registrar_imagenes(imagen, tipo, fecha, hora)
